af2422e53d2ef0816d0ea53bf172e833261d9b2d
Matthieu Bacconnier <neamar@neamar.fr>
2017-06-23 20:29:31 +0100

    Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0);
+		rb.AddForce(moveHorizontal, moveVertical, 0
 	}
 }
