fda09c867f0a7e1ee8a662592a8ff5865bc18307
Matthieu Bacconnier <neamar@neamar.fr>
2017-06-24 12:25:11 +0100

    Display HUD text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -21,6 +28,16 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 7) {
+			winText.text = "YOU WIN!";
 		}
 	}
 }
